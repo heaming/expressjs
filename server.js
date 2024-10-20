9177ac1d664ffaafcb932b71ab8cb442dc5f731a
heaming <meetoohi@gmail.com>
2024-10-21 08:41:55 +0900

    [2024-10-20] express port 연결

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ const express = require('express');
 
 const PORT = 4000;
 
+const Users = [
+    {
+        id: 0,
+        name: 'Jack',
+    },
+    {
+        id: 1,
+        name: 'Jennifer',
+    },
+]
+
 const app = express();
+
+// router setting
+app.get('/users', (req, res) => {
+    res.send(Users);
+})
+
+app.get('/users/:userId', (req, res) => {
+    const userId = Number(req.params.userId);
+    const user = Users[userId];
+    if(user) {
+        res.json(user);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 })
